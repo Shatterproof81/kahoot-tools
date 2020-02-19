10fcf8a64ca23a4c42d3a61bfe0067eedf2145d6
Shatterproof81 <33436779+Shatterproof81@users.noreply.github.com>
2020-02-18 23:02:14 -0500

    Update OptionsForm.vue

--- src/components/OptionsForm.vue
+++ src/components/OptionsForm.vue
@@ -1,11 +1,32 @@
 <template>
   <v-container fluid>
-    <v-select v-model="$globals.options.dark" :items="themeItems" label="Theme"></v-select>
-    <v-select
-      v-model="$globals.options.selectOnAdd"
-      :items="selectOnAddItems"
-      label="Select Group on Add"
-    ></v-select>
+    <v-row>
+      <v-col>
+        <v-select v-model="$globals.options.dark" :items="themeItems" label="Theme"></v-select>
+      </v-col>
+
+      <v-col>
+        <v-select
+          v-model="$globals.options.stealthMode"
+          :items="stealthModeItems"
+          label="Stealth Mode"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-select
+          v-model="$globals.options.selectOnAdd"
+          :items="selectOnAddItems"
+          label="Select Group on Add"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align="center" cols="12">
+        <v-btn color="primary" @click="reset">reset configuration</v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -21,8 +42,18 @@ export default {
       selectOnAddItems: [
         { text: "Yes", value: true },
         { text: "No", value: false }
+      ],
+      stealthModeItems: [
+        { text: "On", value: true },
+        { text: "Off", value: false }
       ]
     };
+  },
+  methods: {
+    reset() {
+      localStorage.clear();
+      location.reload();
+    }
   },
   watch: {
     "$globals.options.dark": {
